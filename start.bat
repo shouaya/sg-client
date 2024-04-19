9145b9bb466307b3fc9993d9b765739e8055ae2a
cc.zhuang <cc.zhuang@weget.jp>
2024-04-20 03:35:47 +0900

    update bat

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
 @echo off
 echo "download python file"
+powershell -Command "pip install 'python-socketio[asyncio_client]'"
+powershell -Command "pip install pyautogui"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/shouaya/sg-client/main/monitor.py -OutFile monitor.py"
 
 echo "excute python file"
